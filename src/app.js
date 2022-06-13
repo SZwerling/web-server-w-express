42d5b8e59e73c831a8ffdffef1a70917703602b3
unknown <sazwerling@gmail.com>
2022-06-13 09:26:02 -0500

    serve up html and json

--- src/app.js
+++ src/app.js
@@ -3,20 +3,26 @@ const express = require('express')  // express is a function, not an object
 const app = express()  // we call the function to create the application
 
 app.get('', (req, res) => {
-    res.send('Hello Express')
+    res.send('<h1>weather</h1>')
 })   // Configure app to respond to different requests
 
 
 app.get('/help', (req, res) => {
-    res.send('Un-Help Desk')
+    res.send([{
+        name: 'slim',
+        age: 46
+    }, 36, 'johnny'])
 })
 
 app.get('/about', (req, res) => {
-    res.send('The About Page')
+    res.send('<h1>this is the about page</h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('The weather page')
+    res.send({
+        location: 'My pants',
+        forecast: 'Lurid'
+    })
 })
 
 app.listen(3000, () => {
