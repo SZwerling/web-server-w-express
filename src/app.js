e3415761dfc277bea295a3c5ec10bc76426aa510
unknown <sazwerling@gmail.com>
2022-06-16 12:00:51 -0500

    use hbs(handlebars) for dynamic templates instead of html files

--- src/app.js
+++ src/app.js
@@ -9,6 +9,30 @@ const app = express()  // we call the function to create the application
 app.use(express.static(path.join(__dirname, '../public'))) // express.static ?? configures application
 // public folder is the only one exposed, that' why we put html and css in there.
 
+app.set('view engine', 'hbs')  // set a value for a given express.js setting (name, value)
+                               // hbs is handlebars for express, to create dynamic templating
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather Index',
+        name: 'Slimmy'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'Weather Index',
+        name: 'Slimmy'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'God helps those who help themselves.'
+    })
+})
+
+
 app.get('/weather', (req, res) => {
     res.send({
         location: 'My pants',
