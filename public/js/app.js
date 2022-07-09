33695b774155e72367d11f835719fc648c1ef29f
unknown <sazwerling@gmail.com>
2022-07-09 15:54:31 -0500

    Browser http req w fetch to weather endpoint

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
-console.log('from js/app.js file')
+fetch("http://localhost:3000/weather?address=Los Angeles").then((response) => {
+   response.json().then((data) => {
+      if (data.error) {
+         console.log(data.error);
+      } else {
+         console.log(data.location)
+         console.log(data.forecast)
+      }
+   });
+});
