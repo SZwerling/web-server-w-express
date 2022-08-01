beaa56abd5dc417e20882bc93a141bab9786158c
unknown <sazwerling@gmail.com>
2022-08-01 18:59:49 -0500

    fetch weather w query selectors and event listeners

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,27 @@
-fetch("http://localhost:3000/weather?address=Los Angeles").then((response) => {
-   response.json().then((data) => {
-      if (data.error) {
-         console.log(data.error);
-      } else {
-         console.log(data.location)
-         console.log(data.forecast)
+// fetch is browser/cliend side. Cannot be used in node.js/server side.
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+
+const fetchWeather = (location) => {
+   fetch(`http://localhost:3000/weather?address=${location}`).then(
+      (response) => {
+         response.json().then((data) => {
+            if (data.error) {
+               console.log(data.error);
+            } else {
+               console.log(data.location);
+               console.log(data.forecast);
+            }
+         });
       }
-   });
+   );
+
+}
+
+weatherForm.addEventListener("submit", (e) => {
+   e.preventDefault();
+   const location = search.value;
+   fetchWeather(location)
+   search.value = ""
 });
