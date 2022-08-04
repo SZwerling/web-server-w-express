2a085c397e16d645f8ba36805d587e799e537ac7
unknown <sazwerling@gmail.com>
2022-08-04 11:04:14 -0500

    connect form and html output to fetch function

--- public/js/app.js
+++ public/js/app.js
@@ -2,16 +2,25 @@
 
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+const message1 = document.querySelector("#message-1")
+const message2 = document.querySelector("#message-2")
+
+
 
 const fetchWeather = (location) => {
+
+   message1.textContent = "...loading"
+   message2.textContent = ""
+
+
    fetch(`http://localhost:3000/weather?address=${location}`).then(
       (response) => {
          response.json().then((data) => {
             if (data.error) {
-               console.log(data.error);
+               message1.textContent = data.error
             } else {
-               console.log(data.location);
-               console.log(data.forecast);
+               message1.textContent = `${data.location}:`
+               message2.textContent = `${data.forecast}.`
             }
          });
       }
